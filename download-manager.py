91ae31d2236de255fbfd568c377b4ed86c1784ec
Bryan Borgesano <BryanBorgesano97@gmail.com>
2019-10-22 11:07:32 -0400

    Created the function movefile to the repeated work of moving each file to a new directory

--- download-manager.py
+++ download-manager.py
@@ -10,17 +10,14 @@ def movefile(source, dest):
     except shutil.Error:
         print(f'{file} already moved')
 
-
 desktop_path = 'C:\\Users\\Bryan\\OneDrive\\Desktop'
 download_path = 'D:\\Downloads'
 programming_path = 'D:\\Programming'
 pdf_path = 'C:\\Users\\Bryan\\OneDrive\\Documents\\All PDFs'
 pictures_path = 'D:\\Pictures'
 picture_extentions = ('.jpeg', '.jpg', '.png','.gif')
-
 video_path = 'D:\\Videos'
 video_extentions = ('.mp4','.wmv', '.mov', '.flv')
-
 zip_path = 'D:\\Downloads\\ZIP Files'
 extracted_zip_path = 'D:\\Downloads\\ZIP Files\\Extracted ZIP Files'
 
@@ -36,60 +33,35 @@ while True:
                 #Pictures
                 if file.lower().endswith(picture_extentions):
                     movefile(f'{download_path}\{file}', pictures_path)
-                    try:
-                        shutil.move(f'{download_path}\{file}', pictures_path)
-                        print(f'Moves {file} to {pictures_path}')
-                    except shutil.Error:
-                        print(f'{file} already moved')
+                    
                 #Videos
                 if file.lower().endswith(video_extentions):
-                    try:
-                        shutil.move(f'{download_path}\{file}', video_path)
-                        print(f'Moves {file} to {video_path}')
-                    except shutil.Error:
-                        print(f'{file} already moved')
+                    movefile(f'{download_path}\{file}', video_path)  
+                   
                 #installs
                 if file.lower().endswith(('.exe', '.msi')):
-                    try:
-                        shutil.move(f'{download_path}\{file}',f'{download_path}\Installs' )
-                        print(f'Moved {file} to D:\Downloads\Installs')
-                    except shutil.Error:
-                        print(f'{file} already moved')
+                    movefile(f'{download_path}\{file}',f'{download_path}\Installs' )
+                
                 #PDF
                 if file.lower().endswith('.pdf'):
-                    try:
-                        shutil.move(f'{download_path}\{file}',f'{pdf_path}' )
-                        print(f'Moved {file} to {programming_path}\Python')
-                    except shutil.Error:
-                        print(f'{file} already moved')
+                    movefile(f'{download_path}\{file}', pdf_path)
+                   
                 #Python
                 if file.lower().endswith('.py'):
-                    try:
-                        shutil.move(f'{download_path}\{file}',f'{programming_path}\Python' )
-                        print(f'Moved {file} to {programming_path}\Python')
-                    except shutil.Error:
-                        print(f'{file} already moved')
+                    movefile(f'{download_path}\{file}',f'{programming_path}\Python' )
+                   
                 #C-Sharp
                 if file.lower().endswith('.cs'):
-                    try:
-                        shutil.move(f'{download_path}\{file}',f'{programming_path}\C#' )
-                        print(f'Moved {file} to {programming_path}\C#')
-                    except shutil.Error:
-                        print(f'{file} already moved')
+                    movefile(f'{download_path}\{file}', f'{programming_path}\C#')
+                   
                 #C++
                 if file.lower().endswith('.h') or file.lower().endswith('.cpp'):
-                    try:
-                        shutil.move(f'{download_path}\{file}',f'{programming_path}\C++' )
-                        print(f'Moved {file} to {programming_path}\C++')
-                    except shutil.Error:
-                        print(f'{file} already moved')
+                    movefile(f'{download_path}\{file}',f'{programming_path}\C++' )
+                
                 #Docx
                 if file.lower().endswith(('.docx','.doc')):
-                    try:
-                        shutil.move(f'{download_path}\{file}',f'{desktop_path}')
-                        print(f'Moved {file} to {programming_path}\C++')
-                    except shutil.Error:
-                        print(f'{file} already moved')
+                    movefile(f'{download_path}\{file}', desktop_path)
+                
                 #Zip Files
                 if file.lower().endswith('.zip'):
                     try:
